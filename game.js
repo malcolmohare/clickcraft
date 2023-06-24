e27c62a1b59111207090f38df0fa1b8a4775b6ce
Malcolm O <malcolmohare@hotmail.com>
2023-06-23 21:08:13 -0400

    item dependencies

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
 const num_rows = 4;
 const num_cols = 4;
 const item_square_edge_size = 80;
-const items = [new Item("log", 5), new Item("wood", 2)];
+const items = [
+  new Item("log", 5),
+  new Item("wood", 2, [{name: "log", quantity: 1}]),
+  new Item("stick", 1, [{name: "wood", quantity: 1}], 4)
+];
 var item_coordinates = [];
+const inventory = {};
 
 function draw(ctx) {
   ctx.fillStyle = "#AAAAAA";
@@ -23,20 +28,24 @@ function main_loop() {
 }
 
 function draw_item(item, item_index, ctx) {
-  x = 25 + item_square_edge_size * item_index;
-  y = 25 + item_square_edge_size * Math.floor(item_index/4);
+  x_padding = y_padding = 0;
+  if (item_index % num_cols > 0) x_padding = 10;
+  if (Math.floor(item_index/num_cols) > 0) y_padding = 10;
+  x = 25 + (item_square_edge_size + x_padding) * item_index;
+  y = 25 + (item_square_edge_size + y_padding) * Math.floor(item_index/num_cols);
   ctx.fillStyle = "#000000";
   ctx.beginPath();
   ctx.rect(x, y, item_square_edge_size, item_square_edge_size);
   ctx.font = "12px Arial";
-  ctx.fillText(item.name + ":" + item.quantity, x, y + 12)
+  ctx.fillStyle = item.can_harvest(inventory) ? "green" : "red";
+  ctx.fillText(item.name + ":" + (inventory[item.name] ?? 0), x, y + 12)
   ctx.stroke();
   console.log("draw item")
   return {x0: x, x1: x + item_square_edge_size, y0: y, y1: y + item_square_edge_size};
 }
 
 function tick_items() {
-  items.forEach(item => item.tick())
+  items.forEach(item => item.tick(inventory))
 }
 
 function mark_click_position(e) {
@@ -51,6 +60,6 @@ function handle_click(e) {
   // detect collision
   item_coordinates.forEach(val => {
     if (val.location.x0 <= x && val.location.x1 >= x && val.location.y0 <= y && val.location.y1 >= y)
-      val.item.click();
+      val.item.click(inventory);
   }); 
 }
--- item.js
+++ item.js
@@ -1,17 +1,21 @@
 class Item {
-  constructor(name, harvest_time) {
+  constructor(name, harvest_time, requirements, quantity) {
     this.name = name;
     this.harvest_time = harvest_time;
     this.harvesting = false;
-    this.quantity = 0;
+    this.quantity = quantity ?? 1;
+    this.requirements = requirements ?? [];
+    this.harvest_time_left = -1;
   }
 
-  tick() {
+  tick(inventory) {
     if (this.harvest_time_left > 0) {
       this.harvest_time_left -= 1
     }
     if (this.harvest_time_left == 0) {
-      this.quantity += 1
+      console.log("harvest finished");
+      inventory[this.name] = (inventory[this.name] ?? 0) + this.quantity;
+      console.log(inventory[this.name]);
       this.harvest_time_left = -1;
     }
   }
@@ -20,8 +24,18 @@ class Item {
     this.harvest_time_left = this.harvest_time;
   }
 
-  click() {
-    this.harvest();
+  click(inventory) {
+    if(this.harvest_time_left == -1) {
+      if (this.can_harvest(inventory)) {
+        this.requirements.forEach(x => inventory[x.name] -= x.quantity);
+        this.harvest();
+      }
+    }
     console.log(this.name + " was clicked!");
+  }
+
+  can_harvest(inventory) {
+    if (this.requirements.some(x => (inventory[x.name] ?? 0) < x.quantity)) return false;
+    return true;
   }
 }
